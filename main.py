a1b81e12564d3ed4dd5f5f8601da0bdc951d10b6
aden kesuma <adenfdfd10@gmail.com>
2023-04-27 20:30:30 +0700

    add sort and search statment

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# buat data siswa secara random
 data = [
     [1, 221151892, 'John Mariano', '7-14-2009','Medan', 'Akutansi', 2015],
     [2, 211112345, 'Jane Maharani', '3-4-2004','Surabaya','Manajemen', 2017],
@@ -10,7 +11,7 @@ data = [
     [9, 181113433, 'Ilham Prasmanan', '2-12-2002','Aceh','Sistem Informasi', 2019]
 ]
 
-def PrintData():
+def PrintData(data):
     column_names = ['No', 'Nim', 'Nama', 'Tanggal Lahir', 'Tempat Lahir', 'Program Studi', 'Tahun Masuk']
     max_widths = [max(len(str(row[i])) for row in data) for i in range(len(column_names))]
     separator = '+-' + '-+-'.join('-' * w for w in max_widths) + '-+'
@@ -24,8 +25,6 @@ def PrintData():
     print(separator)
 
     enter = input()
-    if True:
-        Main()
 
 def AddData():
     print("Tambah Data")
@@ -49,7 +48,6 @@ def ChangeData():
 
     for i in range(len(data)):
         if findNim == data[i][1]:
-            print(data[i][1])
             del data[i]
             nim = int(input("Nim: "))
             nama = input("Nama: ")
@@ -57,16 +55,127 @@ def ChangeData():
             tempatLahir = input("Tempat Lahir: ")
             programStudi = input("Program Studi: ")
             tahunMasuk = int(input("Tahun Masuk: "))
-<<<<<<< HEAD
 
-=======
->>>>>>> e75bd525070297183a647afa2531427cd620943f
             data.insert(i, [i+1, nim, nama, tanggalLahir, tempatLahir, programStudi, tahunMasuk])
 
             print("Data berhasil di ubah")
             enter = input()
             if True:
                 Main()
+
+def SortData(data):
+    def SortByNim(data):
+        n = len(data)
+        for i in range(n - 1):
+            for j in range(n - i - 1):
+                if data[j][1] > data[j + 1][1]:
+                    data[j], data[j + 1] = data[j + 1], data[j]
+        PrintData(data)
+        print("Data Mahasiswa berhasil diurutkan berdasarkan NIM!\n")
+        if True:
+            SortMetode()
+
+    def SortByName(data):
+        n = len(data)
+        for i in range(n - 1):
+            for j in range(n - i - 1):
+                if data[j][2] > data[j + 1][2]:
+                    data[j], data[j + 1] = data[j + 1], data[j]
+        PrintData(data)
+        print("Data Mahasiswa berhasil diurutkan berdasarkan Nama!\n")
+        if True:
+            SortMetode()
+
+    def SortBySubject(data):
+        n = len(data)
+        for i in range(n-1):
+            for j in range(n-i-1):
+                if data[j][5] > data[j+1][5]:
+                    data[j], data[j+1] = data[j+1], data[j]
+        PrintData(data)
+        print("Data Mahasiswa berhasil diurutkan berdasarkan Program Studi!\n")
+        if True:
+            SortMetode()
+
+    def SortByYear(data):
+        n = len(data)
+        for i in range(n-1):
+            for j in range(n-i-1):
+                if data[j][6] > data[j+1][6]:
+                    data[j], data[j+1] = data[j+1], data[j]
+        PrintData(data)
+        print("Data Mahasiswa berhasil diurutkan berdasarkan Tahun Masuk!\n")
+        if True:
+            SortMetode()
+
+    if not data:
+        print("Data Mahasiswa kosong.")
+
+    def SortMetode():
+        print("Pilih metode pengurutan:")
+        print("1. Berdasarkan NIM\n2. Berdasarkan Nama\n3. Berdasarkan Program Studi\n4. Berdasarkan Tahun Masuk\n5. Keluar")
+        choice = int(input("Masukkan pilihan: "))
+
+        if choice == 1:
+            SortByNim(data)
+        elif choice == 2:
+            SortByName(data)
+        elif choice == 3:
+            SortBySubject(data)
+        elif choice == 4:
+            SortByYear(data)
+        elif choice == 5:
+            Main()
+        else:
+            print("Pilihan tidak valid.")
+
+    SortMetode()
+
+def SearchData():
+    def SearchByName(name):
+        result = []
+        for mahasiswa in data:
+            if name.lower() in mahasiswa[2].lower():
+                result.append(mahasiswa)
+        
+        if result:
+            PrintData(result)
+            if True:
+                Main()
+        else:
+            print("Mahasiswa tidak di temukan")
+            Main()
+
+    def SearchByNim(nim):
+        result = []
+        for mahasiswa in data:
+            if str(nim) in str(mahasiswa[1]):
+                result.append(mahasiswa)
+        if result:
+            PrintData(result)
+            if True:
+                Main()
+        else:
+            print("Mahasiswa tidak di temukan")
+            Main()
+        
+
+
+    if not data:
+        print("Data Mahasiswa kosong.")
+
+    print("Pilih metode pencarian:")
+    print("1. Berdasarkan NIM\n2. Berdasarkan Nama")
+    choice = int(input("Masukkan pilihan (1/2): "))
+
+    if choice == 1:
+        nim = int(input("Masukkan NIM Mahasiswa yang dicari: "))
+        mahasiswa = SearchByNim(nim)
+    elif choice == 2:
+        nama = input("Masukkan Nama Mahasiswa yang dicari: ")
+        mahasiswa = SearchByName(nama)
+    else:
+        print("Pilihan tidak valid.")
 
 def Main():
     print("Database Mahasiswa")
@@ -80,14 +189,21 @@ def Main():
         AddData()
     elif choice == 2:
         print("\n")
-        PrintData()
+        PrintData(data)
+        if True:
+            Main()
     elif choice == 3:
         print("\n")
         ChangeData()
+    elif choice == 4:
+        print("\n")
+        SortData(data)
+    elif choice == 5:
+        print("\n")
+        SearchData()
     elif choice == 0:
         print("\nTerimakasih sudah menggunakan program kami")
         quit()
     
-
 Main()
 
--- test.py
+++ test.py
@@ -1,180 +1,181 @@
-class Mahasiswa:
-    def init(self, no, nim, nama, tempat_lahir, tanggal_lahir, program_studi, tahun_masuk):
-        self.no = no
-        self.nim = nim
-        self.nama = nama
-        self.tempat_lahir = tempat_lahir
-        self.tanggal_lahir = tanggal_lahir
-        self.program_studi = program_studi
-        self.tahun_masuk = tahun_masuk
+# class Mahasiswa:
+#     def init(self, no, nim, nama, tempat_lahir, tanggal_lahir, program_studi, tahun_masuk):
+#         self.no = no
+#         self.nim = nim
+#         self.nama = nama
+#         self.tempat_lahir = tempat_lahir
+#         self.tanggal_lahir = tanggal_lahir
+#         self.program_studi = program_studi
+#         self.tahun_masuk = tahun_masuk
 
-    def display(self):
-        print("No: ", self.no)
-        print("NIM: ", self.nim)
-        print("Nama: ", self.nama)
-        print("Tempat Lahir: ", self.tempat_lahir)
-        print("Tanggal Lahir: ", self.tanggal_lahir)
-        print("Program Studi: ", self.program_studi)
-        print("Tahun Masuk: ", self.tahun_masuk)
-        print("\n")
-
-
-def tambah_data_mahasiswa(mahasiswa_list):
-    no = len(mahasiswa_list) + 1
-    nim = input("Masukkan NIM: ")
-    nama = input("Masukkan Nama: ")
-    tempat_lahir = input("Masukkan Tempat Lahir: ")
-    tanggal_lahir = input("Masukkan Tanggal Lahir (DD/MM/YYYY): ")
-    program_studi = input("Masukkan Program Studi: ")
-    tahun_masuk = input("Masukkan Tahun Masuk: ")
-
-    mahasiswa = Mahasiswa(no, nim, nama, tempat_lahir, tanggal_lahir, program_studi, tahun_masuk)
-    mahasiswa_list.append(mahasiswa)
-    print("Data Mahasiswa berhasil ditambahkan!")
+#     def display(self):
+#         print("No: ", self.no)
+#         print("NIM: ", self.nim)
+#         print("Nama: ", self.nama)
+#         print("Tempat Lahir: ", self.tempat_lahir)
+#         print("Tanggal Lahir: ", self.tanggal_lahir)
+#         print("Program Studi: ", self.program_studi)
+#         print("Tahun Masuk: ", self.tahun_masuk)
+#         print("\n")
 
 
-def tampilkan_data_mahasiswa(mahasiswa_list):
-    print("\nDaftar Data Mahasiswa:")
-    for mahasiswa in mahasiswa_list:
-        mahasiswa.display()
+# def tambah_data_mahasiswa(mahasiswa_list):
+#     no = len(mahasiswa_list) + 1
+#     nim = input("Masukkan NIM: ")
+#     nama = input("Masukkan Nama: ")
+#     tempat_lahir = input("Masukkan Tempat Lahir: ")
+#     tanggal_lahir = input("Masukkan Tanggal Lahir (DD/MM/YYYY): ")
+#     program_studi = input("Masukkan Program Studi: ")
+#     tahun_masuk = input("Masukkan Tahun Masuk: ")
+
+#     mahasiswa = Mahasiswa(no, nim, nama, tempat_lahir, tanggal_lahir, program_studi, tahun_masuk)
+#     mahasiswa_list.append(mahasiswa)
+#     print("Data Mahasiswa berhasil ditambahkan!")
 
 
-def ubah_data_mahasiswa(mahasiswa_list):
-    nim = input("Masukkan NIM Mahasiswa yang akan diubah: ")
-    for mahasiswa in mahasiswa_list:
-        if mahasiswa.nim == nim:
-            nama = input("Masukkan Nama: ")
-            tempat_lahir = input("Masukkan Tempat Lahir: ")
-            tanggal_lahir = input("Masukkan Tanggal Lahir (DD/MM/YYYY): ")
-            program_studi = input("Masukkan Program Studi: ")
-            tahun_masuk = input("Masukkan Tahun Masuk: ")
-
-            mahasiswa.nama = nama
-            mahasiswa.tempat_lahir = tempat_lahir
-            mahasiswa.tanggal_lahir = tanggal_lahir
-            mahasiswa.program_studi = program_studi
-            mahasiswa.tahun_masuk = tahun_masuk
-
-            print("Data Mahasiswa berhasil diubah!")
-            return
-
-    print("NIM Mahasiswa tidak ditemukan.")
+# def tampilkan_data_mahasiswa(mahasiswa_list):
+#     print("\nDaftar Data Mahasiswa:")
+#     for mahasiswa in mahasiswa_list:
+#         mahasiswa.display()
 
 
-def bubble_sort_by_nim(mahasiswa_list):
-    n = len(mahasiswa_list)
-    for i in range(n - 1):
-        for j in range(n - i - 1):
-            if mahasiswa_list[j].nim > mahasiswa_list[j + 1].nim:
-                mahasiswa_list[j], mahasiswa_list[j + 1] = mahasiswa_list[j + 1], mahasiswa_list[j]
+# def ubah_data_mahasiswa(mahasiswa_list):
+#     nim = input("Masukkan NIM Mahasiswa yang akan diubah: ")
+#     for mahasiswa in mahasiswa_list:
+#         if mahasiswa.nim == nim:
+#             nama = input("Masukkan Nama: ")
+#             tempat_lahir = input("Masukkan Tempat Lahir: ")
+#             tanggal_lahir = input("Masukkan Tanggal Lahir (DD/MM/YYYY): ")
+#             program_studi = input("Masukkan Program Studi: ")
+#             tahun_masuk = input("Masukkan Tahun Masuk: ")
 
-def bubble_sort_by_nim(mahasiswa_list):
-    n = len(mahasiswa_list)
-    for i in range(n - 1):
-        for j in range(n - i - 1):
-            if mahasiswa_list[j].nim > mahasiswa_list[j + 1].nim:
-                mahasiswa_list[j], mahasiswa_list[j + 1] = mahasiswa_list[j + 1], mahasiswa_list[j]
+#             mahasiswa.nama = nama
+#             mahasiswa.tempat_lahir = tempat_lahir
+#             mahasiswa.tanggal_lahir = tanggal_lahir
+#             mahasiswa.program_studi = program_studi
+#             mahasiswa.tahun_masuk = tahun_masuk
 
+#             print("Data Mahasiswa berhasil diubah!")
+#             return
 
-def bubble_sort_by_nama(mahasiswa_list):
-    n = len(mahasiswa_list)
-    for i in range(n - 1):
-        for j in range(n - i - 1):
-            if mahasiswa_list[j].nama > mahasiswa_list[j + 1].nama:
-                mahasiswa_list[j], mahasiswa_list[j + 1] = mahasiswa_list[j + 1], mahasiswa_list[j]
-
-
-def urutkan_data_mahasiswa(mahasiswa_list):
-    if not mahasiswa_list:
-        print("Data Mahasiswa kosong.")
-        return
-
-    print("Pilih metode pengurutan:")
-    print("1. Berdasarkan NIM")
-    print("2. Berdasarkan Nama")
-    choice = input("Masukkan pilihan (1/2): ")
-
-    if choice == "1":
-        bubble_sort_by_nim(mahasiswa_list)
-        print("Data Mahasiswa berhasil diurutkan berdasarkan NIM!")
-    elif choice == "2":
-        bubble_sort_by_nama(mahasiswa_list)
-        print("Data Mahasiswa berhasil diurutkan berdasarkan Nama!")
-    else:
-        print("Pilihan tidak valid.")
+#     print("NIM Mahasiswa tidak ditemukan.")
 
 
-def linear_search_by_nim(mahasiswa_list, nim):
-    for mahasiswa in mahasiswa_list:
-        if mahasiswa.nim == nim:
-            return mahasiswa
-    return None
+# def bubble_sort_by_nim(mahasiswa_list):
+#     n = len(mahasiswa_list)
+#     for i in range(n - 1):
+#         for j in range(n - i - 1):
+#             if mahasiswa_list[j].nim > mahasiswa_list[j + 1].nim:
+#                 mahasiswa_list[j], mahasiswa_list[j + 1] = mahasiswa_list[j + 1], mahasiswa_list[j]
+
+# def bubble_sort_by_nim(mahasiswa_list):
+#     n = len(mahasiswa_list)
+#     for i in range(n - 1):
+#         for j in range(n - i - 1):
+#             if mahasiswa_list[j].nim > mahasiswa_list[j + 1].nim:
+#                 mahasiswa_list[j], mahasiswa_list[j + 1] = mahasiswa_list[j + 1], mahasiswa_list[j]
 
 
-def linear_search_by_nama(mahasiswa_list, nama):
-    for mahasiswa in mahasiswa_list:
-        if mahasiswa.nama.lower() == nama.lower():
-            return mahasiswa
-    return None
+# def bubble_sort_by_nama(mahasiswa_list):
+#     n = len(mahasiswa_list)
+#     for i in range(n - 1):
+#         for j in range(n - i - 1):
+#             if mahasiswa_list[j].nama > mahasiswa_list[j + 1].nama:
+#                 mahasiswa_list[j], mahasiswa_list[j + 1] = mahasiswa_list[j + 1], mahasiswa_list[j]
 
 
-def cari_data_mahasiswa(mahasiswa_list):
-    if not mahasiswa_list:
-        print("Data Mahasiswa kosong.")
-        return
+# def urutkan_data_mahasiswa(mahasiswa_list):
+#     if not mahasiswa_list:
+#         print("Data Mahasiswa kosong.")
+#         return
 
-    print("Pilih metode pencarian:")
-    print("1. Berdasarkan NIM")
-    print("2. Berdasarkan Nama")
-    choice = input("Masukkan pilihan (1/2): ")
+#     print("Pilih metode pengurutan:")
+#     print("1. Berdasarkan NIM")
+#     print("2. Berdasarkan Nama")
+#     choice = input("Masukkan pilihan (1/2): ")
 
-    if choice == "1":
-        nim = input("Masukkan NIM Mahasiswa yang dicari: ")
-        mahasiswa = linear_search_by_nim(mahasiswa_list, nim)
-        if mahasiswa:
-            print("Data Mahasiswa ditemukan:")
-            mahasiswa.display()
-        else:
-            print("NIM Mahasiswa tidak ditemukan.")
-    elif choice == "2":
-        nama = input("Masukkan Nama Mahasiswa yang dicari: ")
-        mahasiswa = linear_search_by_nama(mahasiswa_list, nama)
-        if mahasiswa:
-            print("Data Mahasiswa ditemukan:")
-            mahasiswa.display()
-        else:
-            print("Nama Mahasiswa tidak ditemukan.")
-    else:
-        print("Pilihan tidak valid.")
-
-def main():
-    mahasiswa_list = []
-
-    while True:
-        print("\nMenu:")
-        print("1. Tambah Data Mahasiswa")
-        print("2. Tampilkan Data Mahasiswa")
-        print("3. Ubah Data Mahasiswa")
-        print("4. Urutkan Data Mahasiswa")
-        print("5. Cari Data Mahasiswa")
-        print("6. Keluar")
-        choice = input("Masukkan pilihan (1/2/3/4/5/6): ")
-
-        if choice == "1":
-            tambah_data_mahasiswa(mahasiswa_list)
-        elif choice == "2":
-            tampilkan_data_mahasiswa(mahasiswa_list)
-        elif choice == "3":
-            ubah_data_mahasiswa(mahasiswa_list)
-        elif choice == "4":
-            urutkan_data_mahasiswa(mahasiswa_list)
-        elif choice == "5":
-            cari_data_mahasiswa(mahasiswa_list)
-        elif choice == "6":
-            print("Terima kasih telah menggunakan program ini!")
-            break
-        else:
-            print("Pilihan tidak valid.")
+#     if choice == "1":
+#         bubble_sort_by_nim(mahasiswa_list)
+#         print("Data Mahasiswa berhasil diurutkan berdasarkan NIM!")
+#     elif choice == "2":
+#         bubble_sort_by_nama(mahasiswa_list)
+#         print("Data Mahasiswa berhasil diurutkan berdasarkan Nama!")
+#     else:
+#         print("Pilihan tidak valid.")
 
 
-main()
+# def linear_search_by_nim(mahasiswa_list, nim):
+#     for mahasiswa in mahasiswa_list:
+#         if mahasiswa.nim == nim:
+#             return mahasiswa
+#     return None
+
+
+# def linear_search_by_nama(mahasiswa_list, nama):
+#     for mahasiswa in mahasiswa_list:
+#         if mahasiswa.nama.lower() == nama.lower():
+#             return mahasiswa
+#     return None
+
+
+# def cari_data_mahasiswa(mahasiswa_list):
+#     if not mahasiswa_list:
+#         print("Data Mahasiswa kosong.")
+#         return
+
+#     print("Pilih metode pencarian:")
+#     print("1. Berdasarkan NIM")
+#     print("2. Berdasarkan Nama")
+#     choice = input("Masukkan pilihan (1/2): ")
+
+#     if choice == "1":
+#         nim = input("Masukkan NIM Mahasiswa yang dicari: ")
+#         mahasiswa = linear_search_by_nim(mahasiswa_list, nim)
+#         if mahasiswa:
+#             print("Data Mahasiswa ditemukan:")
+#             mahasiswa.display()
+#         else:
+#             print("NIM Mahasiswa tidak ditemukan.")
+#     elif choice == "2":
+#         nama = input("Masukkan Nama Mahasiswa yang dicari: ")
+#         mahasiswa = linear_search_by_nama(mahasiswa_list, nama)
+#         if mahasiswa:
+#             print("Data Mahasiswa ditemukan:")
+#             mahasiswa.display()
+#         else:
+#             print("Nama Mahasiswa tidak ditemukan.")
+#     else:
+#         print("Pilihan tidak valid.")
+
+# def main():
+#     mahasiswa_list = []
+
+#     while True:
+#         print("\nMenu:")
+#         print("1. Tambah Data Mahasiswa")
+#         print("2. Tampilkan Data Mahasiswa")
+#         print("3. Ubah Data Mahasiswa")
+#         print("4. Urutkan Data Mahasiswa")
+#         print("5. Cari Data Mahasiswa")
+#         print("6. Keluar")
+#         choice = input("Masukkan pilihan (1/2/3/4/5/6): ")
+
+#         if choice == "1":
+#             tambah_data_mahasiswa(mahasiswa_list)
+#         elif choice == "2":
+#             tampilkan_data_mahasiswa(mahasiswa_list)
+#         elif choice == "3":
+#             ubah_data_mahasiswa(mahasiswa_list)
+#         elif choice == "4":
+#             urutkan_data_mahasiswa(mahasiswa_list)
+#         elif choice == "5":
+#             cari_data_mahasiswa(mahasiswa_list)
+#         elif choice == "6":
+#             print("Terima kasih telah menggunakan program ini!")
+#             break
+#         else:
+#             print("Pilihan tidak valid.")
+
+
+# main()
+
