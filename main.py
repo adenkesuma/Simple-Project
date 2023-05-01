3d7d074c0654a903a17ed488bed265753d788364
aden kesuma <adenfdfd10@gmail.com>
2023-05-01 22:47:23 +0700

    adding to the complexity of the algorithm

--- main.py
+++ main.py
@@ -8,25 +8,32 @@ data = [
     [6, 221413433, 'Bernard Sinaga', '2-12-2002','Lamongan','Teknologi Informasi', 2019],
     [7, 171156892, 'Dino Maharudi', '7-14-2009','Madiun', 'Akutansi', 2015],
     [8, 191111234, 'Mesa Hibriji', '3-4-2004','Bali','Manajemen', 2017],
-    [9, 181113433, 'Ilham Prasmanan', '2-12-2002','Aceh','Sistem Informasi', 2019]
+    [9, 181113433, 'Ilham Prasmanan', '2-12-2002','Aceh','Sistem Informasi', 2019],
+    [10, 221111805, 'Aden Kesuma', '14-08-2004', 'Medan', 'Teknik Informatika', 2022]
 ]
 
+# fungsi printData adalah fungsi yang bertugas untuk menampilkan data siswa
 def PrintData(data):
+    # kompleksitas waktu: O(n)
+    # karena perulangan sebanyak N kali untuk menampilkan setiap baris, dan untuk setiap baris perlu melihat setiap kolom.
     column_names = ['No', 'Nim', 'Nama', 'Tanggal Lahir', 'Tempat Lahir', 'Program Studi', 'Tahun Masuk']
     max_widths = [max(len(str(row[i])) for row in data) for i in range(len(column_names))]
-    separator = '+-' + '-+-'.join('-' * w for w in max_widths) + '-+'
+    separator = '+-' + '-+-'.join('-' * w for w in max_widths[:3] + [max(max_widths[3], 14), max(max_widths[4], 12), max(max_widths[5], 15), max(max_widths[6], 11)]) + '-+'
 
     print(separator)
-    print('| ' + ' | '.join('{:<{}}'.format(name, max_widths[i]) for i, name in enumerate(column_names)) + ' |')
+    print('| ' + ' | '.join('{:<{}}'.format(name, max_widths[i]) for i, name in enumerate(column_names[:3])) + ' | ' + '{:<{}}'.format(column_names[3], max(max_widths[3], 14)) + ' | ' + '{:<{}}'.format(column_names[4], max(max_widths[4], 12)) + ' | ' + '{:<{}}'.format(column_names[5], max(max_widths[5], 15)) + ' | ' + '{:<{}}'.format(column_names[6], max(max_widths[6], 11)) + ' |')
     print(separator)
 
     for row in data:
-        print('| ' + ' | '.join('{:<{}}'.format(str(val), max_widths[i]) for i, val in enumerate(row)) + ' |')
+        print('| ' + ' | '.join('{:<{}}'.format(str(val), max_widths[i]) for i, val in enumerate(row[:3])) + ' | ' + '{:<{}}'.format(str(row[3]), max(max_widths[3], 14)).replace(",", ",\n") + ' | ' + '{:<{}}'.format(str(row[4]), max(max_widths[4], 12)).replace(",", ",\n") + ' | ' + '{:<{}}'.format(str(row[5]), max(max_widths[5], 15)).replace(",", ",\n") + ' | ' + '{:<{}}'.format(str(row[6]), max(max_widths[6], 11)).replace(",", ",\n") + ' |')
     print(separator)
 
     enter = input()
 
-def AddData():
+# fungsi addData adalah fungsi yang bertugas untuk menambah data baru 
+def AddData(data):
+    # kompleksitas waktu: O(1)
+    # karena hanya menambahkan satu baris data pada akhir list, yang tidak tergantung pada ukuran data yang sudah ada.
     print("Tambah Data")
     print("="*30)
     nim = int(input("Nim\t\t: "))
@@ -37,12 +44,14 @@ def AddData():
     tahunMasuk = int(input("Tahun Masuk\t: "))
     data.append([len(data)+1, nim, nama, tanggalLahir, tempatLahir, programStudi, tahunMasuk])
 
-    print("Data berhasil di tambahkan")
-    enter = input()
+    print("Data berhasil di tambahkan\n")
     if True:
         Main()
 
-def ChangeData():
+# fungsi changeData adalah fungsi yang bertugas untuk mengubah data yang sebelumnya menjadi data baru
+def ChangeData(data):
+    # kompleksitas waktu: O(n)
+    # karena mencari baris yang sesuai dengan NIM membutuhkan perulangan sebanyak N kali
     findNim = int(input("Masukan Nim : "))
     print("="*30)
 
@@ -62,8 +71,14 @@ def ChangeData():
             enter = input()
             if True:
                 Main()
-
+    else:
+        print(f'Mahasiswa dengan nim {findNim} tidak di temukan, tolong masukan nim yang sesuai')
+        ChangeData(data)    
+        
+# fungsi sortData adalah fungsi yang hanya bertugas untuk menyortir data atau mengurutkan data
 def SortData(data):
+    # kompleksitas waktu: O(n^2)
+    # dikarenakan terdapat dua buah perulangan for bersarang yang menjalankan operasi perbandingan dan penukaran elemen pada array sebanyak n-1 kali pada setiap iterasi.
     def SortByNim(data):
         n = len(data)
         for i in range(n - 1):
@@ -131,11 +146,14 @@ def SortData(data):
 
     SortMetode()
 
-def SearchData():
+# fungsi searchData adalah fungsi yang bertugas khusus mencari sebuah data
+def SearchData(data):
+    # kompleksitas waktu: O(n)
+    # karena hanya menjalankan satu perulangan for dengan operasi perbandingan sebanyak n kali
     def SearchByName(name):
         result = []
         for mahasiswa in data:
-            if name.lower() in mahasiswa[2].lower():
+            if name in mahasiswa[2]:
                 result.append(mahasiswa)
         
         if result:
@@ -177,7 +195,11 @@ def SearchData():
     else:
         print("Pilihan tidak valid.")
 
+# function main adalah fungsi yang pertama kali muncul ketika kode di jalankan
 def Main():
+    '''
+        Kompleksitas waktu dari fungsi ini tergantung pada urutan pemanggilan fungsi dan logika program yang terkait.
+    '''
     print("Database Mahasiswa")
     print("="*18)
     print("1. Tambah \n2. Tampilkan \n3. Ubah \n4. Urutkan \n5. Cari \n0. Keluar")
@@ -186,7 +208,7 @@ def Main():
 
     if choice == 1:
         print("\n")
-        AddData()
+        AddData(data)
     elif choice == 2:
         print("\n")
         PrintData(data)
@@ -194,16 +216,23 @@ def Main():
             Main()
     elif choice == 3:
         print("\n")
-        ChangeData()
+        ChangeData(data)
     elif choice == 4:
         print("\n")
         SortData(data)
     elif choice == 5:
         print("\n")
-        SearchData()
+        SearchData(data)
     elif choice == 0:
         print("\nTerimakasih sudah menggunakan program kami")
         quit()
     
 Main()
 
+
+'''
+    Dalam hal ini, kompleksitas terbesar terdapat pada bagian O(N*M) 
+    dengan kompleksitas waktu O(N*M) karena memiliki perulangan sebanyak N kali
+    untuk menampilkan setiap baris, dan untuk setiap baris perlu melihat setiap kolom.
+    Jadi, kompleksitas keseluruhan dari algoritma ini adalah O(N*M).
+'''
